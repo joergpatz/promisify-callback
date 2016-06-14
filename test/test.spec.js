a06b6800385c423c5833869bec20eb0b27c2719b
Joerg <joerg.patzer@me.com>
2016-06-14 22:25:00 +0200

    add tests

--- test/test.spec.js
+++ test/test.spec.js
@@ -3,15 +3,35 @@ import assert from 'assert';
 import fs from 'fs';
 import promisify from './..';
 
-describe('Basic', () => {
+const test_compare = {
+    "name": "Homer Simpson",
+    "information": {
+        "voiced": "Dan Castellaneta",
+        "occupation": "Safety Inspector at Springfield Nuclear Power Plant"
+    }
+};
+
+describe('Basic Examples', () => {
     describe('#fs.stat', () => {
         it('should access file', () => {
-            return promisify(fs.stat, './test/test.json').then(stat => {
-                assert.equal(stat.isFile(), true);
-                return stat;
-            }).then(stat => {
-                assert.equal(stat.isDirectory(), false);
-            });
+            return promisify(fs.stat, './test/test.json')
+                .then(stat => {
+                    assert.equal(stat.isFile(), true);
+                    return stat;
+                });
+        });
+    });
+    describe('#fs.readFile', () => {
+        it('should readfile and extract name', () => {
+            return promisify(fs.readFile, './test/test.json', 'utf8')
+                .then(JSON.parse)
+                .then(file => {
+                    assert.deepEqual(file, test_compare);
+                    return file.name;
+                }).then(name => {
+                    assert.equal(name, "Homer Simpson");
+                    return name;
+                });
         });
     });
 });
